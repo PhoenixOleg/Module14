d79b81390ffaed2239701b5b42eed561604aceb4
PhoenixOleg <metall_2000@mail.ru>
2023-11-12 00:52:57 +0300

    Отображение контента вынесено в метод Show Перекур)

--- Module14/DisplayContent.cs
+++ Module14/DisplayContent.cs
@@ -18,7 +18,6 @@ namespace Module14
         {
             Settings settings = new();
             int recordsPerPage;
-            string input;
 
             //Рассчитываем минимальное количество записей на страницу из расчета, что максимальное количество страниц (почему-то, например, по ТЗ) равно 9
             int minRecordsPerPage = (int)Math.Ceiling((double)RecordsCount / 9.0);
@@ -38,6 +37,42 @@ namespace Module14
             settings.maxPageNumber = (int)Math.Ceiling((double)RecordsCount / settings.recordsPerPage);
 
             return settings;
+        }
+
+        internal static void Show(List<Contact> phoneBook, Settings settings)
+        {
+            while (true)
+            {
+                // Читаем введенный с консоли символ
+                Console.WriteLine($"\nВыберите номер страницы для отображения контента (от 1 до {settings.maxPageNumber}) или нажмите Esc для выхода");
+
+                var input = Console.ReadKey().KeyChar;
+
+                if (input == 27) //Если Escape - выходим
+                {
+                    break;
+                }
+
+                // Проверяем, число ли это и если не соответствует критериям - показываем ошибку
+                if (!Int32.TryParse(input.ToString(), out int pageNumber) || pageNumber < 1 || pageNumber > settings.maxPageNumber)
+                {
+                    Console.WriteLine("\nСтраницы не существует");
+                }
+                // если соответствует - запускаем вывод
+                else
+                {
+                    // Сначала сортируем, затем пропускаем нужное количество элементов и берем 2 для показа на странице
+                    var pageContent = phoneBook.OrderBy(x => x.Name).ThenBy(x => x.LastName).Skip((pageNumber - 1) * settings.recordsPerPage).Take(settings.recordsPerPage);
+                    Console.WriteLine();
+
+                    // выводим результат
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+
+                    Console.WriteLine();
+                }
+            }
+
         }
     }
 }
--- Module14/Program.cs
+++ Module14/Program.cs
@@ -17,42 +17,14 @@
             #endregion Добавляем контакты
 
 
-            
+
             //int recordsOnPage = 0; //Количество записей на страницу для отображения
             //int maxPageNumber; //Максимальное количество доступных страниц, исходя из значения recordsOnPage
 
-            Settings settings = DisplayContent.PrepareFormat(phoneBook.Count);
-
             while (true)
             {
-                // Читаем введенный с консоли символ
-                Console .WriteLine($"\nВыберите номер страницы для отображения контента (от 1 до {settings.maxPageNumber}) или нажмите Esc для выхода");
-                
-                var input = Console.ReadKey().KeyChar;
-
-                if (input == 27) //Если Escape - выходим
-                {
-                    Final.GoodBye(0, "");
-                }
-
-                // Проверяем, число ли это и если не соответствует критериям - показываем ошибку
-                if (!Int32.TryParse(input.ToString(), out int pageNumber) || pageNumber < 1 || pageNumber > settings.maxPageNumber)
-                {
-                    Console.WriteLine("\nСтраницы не существует");
-                }
-                // если соответствует - запускаем вывод
-                else
-                {
-                    // Сначала сортируем, затем пропускаем нужное количество элементов и берем 2 для показа на странице
-                    var pageContent = phoneBook.OrderBy(x => x.Name).ThenBy(x => x.LastName).Skip((pageNumber - 1) * settings.recordsPerPage).Take(settings.recordsPerPage);
-                    Console.WriteLine();
-
-                    // выводим результат
-                    foreach (var entry in pageContent)
-                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
-
-                    Console.WriteLine();
-                }
+                Settings settings = DisplayContent.PrepareFormat(phoneBook.Count);
+                DisplayContent.Show(phoneBook, settings);
             }
         }
     }
